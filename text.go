49a55044c954d442d8a80ec34b58608ac0e717c4
David Lam <david.lam@winc.com.au>
2020-09-01 23:47:40 +1000

    commit

--- text.go
+++ text.go
@@ -2,14 +2,9 @@ package text
 
 import (
 	"encoding/base64"
-	"encoding/hex"
-	"math/big"
+
 	"regexp"
 	"strings"
-)
-
-const (
-	encodeEWM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{}"
 )
 
 var (
@@ -18,7 +13,6 @@ var (
 	reDigits4To15  *regexp.Regexp
 	reAlphaNum7    *regexp.Regexp
 	reAlphaNum8To9 *regexp.Regexp
-	EWMEncoding    *base64.Encoding
 )
 
 func init() {
@@ -27,7 +21,7 @@ func init() {
 	reDigits4To15 = regexp.MustCompile("^[0-9]{4,15}$")
 	reAlphaNum7 = regexp.MustCompile("^[A-Z0-9]{7,7}$")
 	reAlphaNum8To9 = regexp.MustCompile("^[A-Z0-9]{8,9}$")
-	EWMEncoding = base64.NewEncoding(encodeEWM).WithPadding(base64.NoPadding)
+
 }
 
 func ValidAlphaNum8(s string) bool {
@@ -83,46 +77,4 @@ func Base64Decode(encoded string) string {
 	}
 	return string(decoded)
 
-}
-
-// UUID, or GUID, is a 128 bit number used as an ID number. The same number can be represented in different forms.
-//
-// EWMEncodeUUID() converts hexadecimal to a non-standard base64 used by the EWM system.
-//
-// EWMDecodeUUID() converts the the non-standard base64 to hexadecimal.
-func EWMEncodeUUID(hx string) string {
-	b, err := hex.DecodeString(hx)
-	if err != nil {
-		return ""
-	}
-	padding := make([]byte, 16-len(b))
-	return EWMEncoding.EncodeToString(append(padding, b...))
-}
-
-// UUID, or GUID, is a 128 bit number used as an ID number. The same number can be represented in different forms.
-//
-// EWMEncodeUUID() converts hexadecimal to a non-standard base64 used by the EWM system.
-//
-// EWMDecodeUUID() converts the the non-standard base64 to hexadecimal.
-func EWMDecodeUUID(c22 string) string {
-	b, _ := EWMEncoding.DecodeString(c22)
-	s := hex.EncodeToString(b)
-	return strings.Repeat("0", 32-len(s)) + strings.ToUpper(s)
-}
-
-// slightly slower
-func EWMEncodeUUIDSlower(hex string) string {
-	bigInt := new(big.Int)
-	bigInt.SetString(hex, 16)
-	padding := make([]byte, 16-len(bigInt.Bytes()))
-	return EWMEncoding.EncodeToString(append(padding, bigInt.Bytes()...))
-}
-
-// not tested for speed but expect to be slightly slower
-func EWMDecodeUUIDSlower(c22 string) string {
-	b, _ := EWMEncoding.DecodeString(c22)
-	bigInt := new(big.Int)
-	bigInt.SetBytes(b)
-	s := strings.ToUpper(bigInt.Text(16))
-	return strings.Repeat("0", 32-len(s)) + s
 }
