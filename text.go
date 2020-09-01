83d4c6092e23274da81136a31c712b6b52fe3fef
David Lam <david.lam@winc.com.au>
2020-09-01 23:38:35 +1000

    used hex package instead of bigInt

--- text.go
+++ text.go
@@ -2,6 +2,7 @@ package text
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"math/big"
 	"regexp"
 	"strings"
@@ -89,11 +90,13 @@ func Base64Decode(encoded string) string {
 // EWMEncodeUUID() converts hexadecimal to a non-standard base64 used by the EWM system.
 //
 // EWMDecodeUUID() converts the the non-standard base64 to hexadecimal.
-func EWMEncodeUUID(hex string) string {
-	bigInt := new(big.Int)
-	bigInt.SetString(hex, 16)
-	padding := make([]byte, 16-len(bigInt.Bytes()))
-	return EWMEncoding.EncodeToString(append(padding, bigInt.Bytes()...))
+func EWMEncodeUUID(hx string) string {
+	b, err := hex.DecodeString(hx)
+	if err != nil {
+		return ""
+	}
+	padding := make([]byte, 16-len(b))
+	return EWMEncoding.EncodeToString(append(padding, b...))
 }
 
 // UUID, or GUID, is a 128 bit number used as an ID number. The same number can be represented in different forms.
@@ -102,6 +105,21 @@ func EWMEncodeUUID(hex string) string {
 //
 // EWMDecodeUUID() converts the the non-standard base64 to hexadecimal.
 func EWMDecodeUUID(c22 string) string {
+	b, _ := EWMEncoding.DecodeString(c22)
+	s := hex.EncodeToString(b)
+	return strings.Repeat("0", 32-len(s)) + strings.ToUpper(s)
+}
+
+// slightly slower
+func EWMEncodeUUIDSlower(hex string) string {
+	bigInt := new(big.Int)
+	bigInt.SetString(hex, 16)
+	padding := make([]byte, 16-len(bigInt.Bytes()))
+	return EWMEncoding.EncodeToString(append(padding, bigInt.Bytes()...))
+}
+
+// not tested for speed but expect to be slightly slower
+func EWMDecodeUUIDSlower(c22 string) string {
 	b, _ := EWMEncoding.DecodeString(c22)
 	bigInt := new(big.Int)
 	bigInt.SetBytes(b)
--- text_test.go
+++ text_test.go
@@ -42,13 +42,14 @@ func TestEWMEncodeUUID(t *testing.T) {
 		"4C419E52B069732EE10000000A141F1F": "J46UKh1fSoxX00002XGV7m",
 		"91AD714ECF7C7571E10000000A141F31": "aQrnJizyTN7X00002XGVCG",
 	}
-
-	for k, v := range data {
-		t.Run("", func(t *testing.T) {
-			if got := EWMEncodeUUID(k); got != v {
-				t.Errorf("EWMDecodeUUID() = %v, want %v", got, v)
-			}
-		})
+	for i := 0; i < 100000; i++ {
+		for k, v := range data {
+			t.Run("", func(t *testing.T) {
+				if got := EWMEncodeUUID(k); got != v {
+					t.Errorf("EWMDecodeUUID() = %v, want %v", got, v)
+				}
+			})
+		}
 	}
 }
 
@@ -68,5 +69,26 @@ func TestEWMDecodeUUID(t *testing.T) {
 				t.Errorf("EWMDecodeUUID() = %v, want %v", got, k)
 			}
 		})
+	}
+}
+
+func TestEWMEncodeUUIDSlower(t *testing.T) {
+
+	data := map[string]string{
+		"005056B31E4F1EE48398F7FA866FCB15": "051MinvF7kI3cFVwXc}B5G",
+		"005056B33BF11EE3BAC398355F449E72": "051Mipln7kEwmvWrNqIUSW",
+		"005056B3483B1ED487A6145349669844": "051MiqWx7jI7fXHJIMQOH0",
+		"4C419E52B069732EE10000000A141F1F": "J46UKh1fSoxX00002XGV7m",
+		"91AD714ECF7C7571E10000000A141F31": "aQrnJizyTN7X00002XGVCG",
+	}
+
+	for i := 0; i < 100000; i++ {
+		for k, v := range data {
+			t.Run("", func(t *testing.T) {
+				if got := EWMEncodeUUIDSlower(k); got != v {
+					t.Errorf("EWMDecodeUUIDSlower() = %v, want %v", got, v)
+				}
+			})
+		}
 	}
 }
